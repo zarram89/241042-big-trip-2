f5f3de27fabc6754c53d371fb8a80e14ef4e2f74
Ramil <zarram89@gmail.com>
2026-06-08 19:44:13 +0500

    Добавляет компоненты с разметкой

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { render } from './render.js';
+import FilterView from './view/filter-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
+
+const siteHeaderElement = document.querySelector('.page-header');
+const siteFilterElement = siteHeaderElement.querySelector('.trip-controls__filters');
+const siteMainElement = document.querySelector('.page-main');
+const siteBoardElement = siteMainElement.querySelector('.trip-events');
+
+const boardPresenter = new BoardPresenter({ boardContainer: siteBoardElement });
+
+render(new FilterView(), siteFilterElement);
+
+boardPresenter.init();
